debe77cc524d816109db5ae50ff737ae32f92f72
Janan Akkad <jananakkad@hotmail.co.uk>
2022-11-17 18:29:29 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,14 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
+
+  # def tags_attributes=(tag_attributes)
+  #   tag_attributes.values.each do |tag_attributes|
+  #     if tag_attributes["name"].present? 
+  #     tag = Tag.find_or_create_by(tag_attributes)
+  #     self.post_tags.build(tag: tag)
+  #     end
+  #   end
+  # end
 end
